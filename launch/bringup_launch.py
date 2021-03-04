79108b6f3a528c41f809c056f6fa305cd9196162
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-03-04 19:38:00 +0700

    Remove bad quotes

--- launch/bringup_launch.py
+++ launch/bringup_launch.py
@@ -30,19 +30,19 @@ def generate_launch_description():
     model_path, plugin_path, media_path = GazeboRosPaths.get_paths()
 
     env = {
-        "GAZEBO_MODEL_PATH": model_path,
-        "GAZEBO_PLUGIN_PATH": plugin_path,
-        "GAZEBO_RESOURCE_PATH": media_path,
+        'GAZEBO_MODEL_PATH': model_path,
+        'GAZEBO_PLUGIN_PATH': plugin_path,
+        'GAZEBO_RESOURCE_PATH': media_path,
     }
 
-    world_prefix = get_package_share_directory("dienen_gazebo_sim")
-    world_file = os.path.join(world_prefix, "worlds", "default.sdl")
+    world_prefix = get_package_share_directory('dienen_gazebo_sim')
+    world_file = os.path.join(world_prefix, 'worlds', 'default.sdl')
 
     return LaunchDescription(
         [
             ExecuteProcess(
-                cmd=["gazebo", world_file],
-                output="screen",
+                cmd=['gazebo', world_file],
+                output='screen',
                 additional_env=env,
             )
         ]
