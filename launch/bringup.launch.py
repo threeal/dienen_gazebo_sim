3608c0c2907c760dddd17fa2a73cf68285958c66
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-04-28 16:07:01 +0700

    feat: modify the bringup launch file

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -20,29 +20,29 @@
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
-import os
-from scripts import GazeboRosPaths
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    model_path, plugin_path, media_path = GazeboRosPaths.get_paths()
-
-    env = {
-        'GAZEBO_MODEL_PATH': model_path,
-        'GAZEBO_PLUGIN_PATH': plugin_path,
-        'GAZEBO_RESOURCE_PATH': media_path,
-    }
-
-    package_prefix = get_package_share_directory('dienen_gazebo_sim')
-    world_file = os.path.join(package_prefix, 'worlds', 'default.world')
-
-    return LaunchDescription(
-        [
-            ExecuteProcess(
-                cmd=['gazebo', '--verbose', world_file],
-                output='screen',
-                additional_env=env,
-            ),
-        ]
-    )
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'world', default_value='default.world',
+            description='Specify world file name'
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                get_package_share_directory('gazebo_ros'),
+                '/launch/gazebo.launch.py'
+            ]),
+            launch_arguments=[
+                ('world', [
+                    get_package_share_directory('dienen_gazebo_sim'),
+                    '/worlds/',
+                    LaunchConfiguration('world')
+                ]),
+                ('verbose', 'true')
+            ]
+        )
+    ])
